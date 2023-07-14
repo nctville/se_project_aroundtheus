16c77dc6c522fa1e93d8408c86024b55b472a819
NickT1000 <nctville@live.com>
2023-07-14 12:16:23 -0400

    created javascript array w/ objects

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const cardObject1= {
+    title: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+const cardObject2={
+    title: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+const cardObject3={
+    title: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+const cardObject4={
+    title: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+const cardObject5={
+    title: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+const cardObject6={
+    title: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+};
+
+const initalCards=[cardObject1, cardObject2, cardObject3, cardObject4, cardObject5, cardObject6];
+console.log(initalCards);
